1f0d31f58559677ec47e05fe46f415af5de7263c
cre4tive97 <harryborrison5148@gmail.com>
2021-10-15 14:00:33 +0900

    shuffleImage, createImageTiles

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ const gameFinish = document.querySelector(".game-finish");
 const playtime = document.querySelector(".container__playtime");
 
 let tileCount = 16;
+let tiles = [];
 
-Array(tileCount)
-  .fill()
-  .forEach((a, i) => {
-    const li = document.createElement("li");
-    li.setAttribute("data-index", i);
-    li.classList.add(`list${i}`);
-    container.appendChild(li);
-  });
+tiles = createImageTiles();
+
+shuffleImage(tiles.forEach((tile) => container.appendChild(tile)));
+
+function shuffleImage(arr) {
+  let index = arr.length - 1;
+  while (index > 0) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    [arr[index], arr[randomIndex]] = [arr[randomIndex], arr[index]];
+    index--;
+  }
+  return arr;
+}
+
+function createImageTiles() {
+  const tempArray = [];
+  Array(tileCount)
+    .fill()
+    .forEach((a, i) => {
+      const li = document.createElement("li");
+      li.setAttribute("data-index", i);
+      li.classList.add(`list${i}`);
+      tempArray.push(li);
+    });
+  return tempArray;
+}
